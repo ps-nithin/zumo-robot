d5d4765114ece9301ba0b417aa2590e290d9e640
nithin ps <62709849+ps-nithin@users.noreply.github.com>
2022-11-25 21:27:21 +0530

    Delete zumo directory

--- zumo/get_image.php
+++
@@ -1,8 +0,0 @@
-<?php
-system("raspistill -w 320 -h 320 -rot 180 -o first.jpg");
-$c1='convert -pointsize 10 -fill yellow -draw "text 90,310 ';
-$date=system("date");
-$c2='\'Last updated on '.$date.'\'';
-$c3='" first.jpg second.jpg';
-system($c1.$c2.$c3);
-?>
--- zumo/voice.php
+++
@@ -1,4 +0,0 @@
-<?php
-$voice=$_FILES['audio_data']['tmp_name'];
-system("cvlc $voice");
-?>
