a109af2c4d3443ebe61f997c8cf2a717f81c8736
nithin ps <62709849+ps-nithin@users.noreply.github.com>
2022-11-25 21:26:44 +0530

    Delete html directory

--- html/play_music.php
+++
@@ -1,5 +0,0 @@
-<?php
-$link=$_GET['ytlink'];
-echo $link;
-system("youtube-dl -g --format bestaudio $link | cvlc - vlc://quit");
-?>
--- html/stop.php
+++
@@ -1,6 +0,0 @@
-<?php
-system("pigs w 23 0");
-system("pigs w 22 0");
-system("pigs w 24 0");
-system("pigs w 27 0");
-?>
