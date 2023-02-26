7e88730d4763cad3435d240872e5acbf35392a2f
nithin ps <62709849+ps-nithin@users.noreply.github.com>
2023-02-26 17:48:54 +0530

    Add files via upload

--- zumo-web/forward.php
+++ zumo-web/forward.php
@@ -1,14 +1,30 @@
 <?php
 if(isset($_GET['time'])){
-  $useconds=abs($_GET['time'])/1000000.0;
+  $useconds=abs($_GET['time'])*1000000.0;
 }else{
   $useconds=1000000;
 }  
-system("pigs w 23 1");
-system("pigs w 27 1");
-system("pigs w 24 0");
-system("pigs w 22 0");
+if(isset($_GET['dutycycle'])){
+  $dutycycle=$_GET['dutycycle'];
+}else{
+  $dutycycle=255;
+}
+system("pigs pfs 23 50");
+system("pigs pfs 27 50");
+system("pigs pfs 22 50");
+system("pigs pfs 24 50");
+system("pigs p 23 $dutycycle");
+system("pigs p 27 $dutycycle");
+system("pigs p 24 0");
+system("pigs p 22 0");
 usleep($useconds);
-system("pigs w 23 0");
-system("pigs w 27 0");
+system("pigs p 23 0");
+system("pigs p 27 0");
+$UPTIME_MOTORS="/home/pi/uptime_motors.txt";
+$utfile=fopen($UPTIME_MOTORS,'r');
+$ut_pre=fread($utfile,filesize($UPTIME_MOTORS));
+fclose($utfile);
+$utfile=fopen($UPTIME_MOTORS,'w');
+fwrite($utfile,$ut_pre+$useconds/1000000);
+fclose($utfile);
 ?>
--- zumo-web/spin.php
+++ zumo-web/spin.php
@@ -1,21 +1,41 @@
 <?php
-$angle=$_GET['angle'];
-$t=abs($angle*5555);
-system("pigs w 23 0");
-system("pigs w 22 0");
-system("pigs w 24 0");
-system("pigs w 27 0");
-if ($angle>0){
-system("pigs w 23 1");
-system("pigs w 22 1");
-usleep($t);
-system("pigs w 23 0");
-system("pigs w 22 0");
-}else if ($angle<0){
-system("pigs w 24 1");
-system("pigs w 27 1");
-usleep($t);
-system("pigs w 24 0");
-system("pigs w 27 0");
+$UPTIME_MOTORS="/home/pi/uptime_motors.txt";
+if(isset($_GET['angle'])){
+  $angle=$_GET['angle'];
+}else{
+  $angle=20;
+}  
+if(!isset($_GET['dutycycle'])){
+  $dutycycle=255;
+}else{
+  $dutycycle=$_GET['dutycycle'];
 }
+system("pigs pfs 23 50");
+system("pigs pfs 22 50");
+system("pigs pfs 24 50");
+system("pigs pfs 27 50");
+$t=abs($angle*5555);
+system("pigs p 23 0");
+system("pigs p 22 0");
+system("pigs p 24 0");
+system("pigs p 27 0");
+if ($angle>0){
+system("pigs p 23 $dutycycle");
+system("pigs p 22 $dutycycle");
+usleep($t);
+system("pigs p 23 0");
+system("pigs p 22 0");
+}else if ($angle<0){
+system("pigs p 24 $dutycycle");
+system("pigs p 27 $dutycycle");
+usleep($t);
+system("pigs p 24 0");
+system("pigs p 27 0");
+}
+$utfile=fopen($UPTIME_MOTORS,'r');
+$ut_pre=fread($utfile,filesize($UPTIME_MOTORS));
+fclose($utfile);
+$utfile=fopen($UPTIME_MOTORS,'w');
+fwrite($utfile,$ut_pre+$t/1000000);
+fclose($utfile);
 ?>
